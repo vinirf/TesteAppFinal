8fe3d13bd18c65ce0f04ed8ff67a615352ec3e72
vinirf <vinicius_resendewii@hotmail.com>
2014-05-09 17:14:32 -0300

    leiutura xml

---
+++ TesteAppFinal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TesteAppFinal
+//
+//  Created by VINICIUS RESENDE FIALHO on 09/05/14.
+//  Copyright (c) 2014 VINICIUS RESENDE FIALHO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
